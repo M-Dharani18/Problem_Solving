1fc2c35469c4dd6acdb6b5b467d9c0e4ddd3098a
Dharani M <dharani39393@gmail.com>
2025-08-19 23:36:23 +0530

    Time: 1 ms (99.14%), Space: 44.7 MB (84.28%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,18 +1,12 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
          int[] freq1= new int[26];
-         char [] m = ransomNote.toCharArray();
-         int n=m.length;
-         for(char i : ransomNote.toCharArray()){
-            freq1[i-'a']++;
+         for(char i : magazine.toCharArray()){
+            ++freq1[i-'a'];
          }
-         for(char j : magazine.toCharArray()){
-            freq1[j-'a']--;
-         }
-         for(int i=0;i<26;i++){
-            if(freq1[i]>0)
-            return false;
-         }
+         for(char j : ransomNote.toCharArray())
+            if( --freq1[j-'a'] < 0)
+                return false;
          return true;
         }
 }
