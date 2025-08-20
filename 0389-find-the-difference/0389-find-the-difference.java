e7229ed2040abd1240ded44e2723b89889b6fcc9
Dharani M <dharani39393@gmail.com>
2025-08-20 12:11:44 +0530

    Time: 2 ms (62.7%), Space: 42.2 MB (11.64%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,18 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int[] freq=new int[26];
+        char[] c1 = s.toCharArray();
+        char[] c2 = t.toCharArray();
+        for(char c : c1){
+            freq[c-'a']++;
+        }
+        for(char c : c2){
+            freq[c-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(freq[i]==-1)
+            return (char)('a'+i);
+        }
+        return 0;
+    }
+}
