da750a273a94611c3535fbb062505ceec4933b86
Dharani M <dharani39393@gmail.com>
2025-08-25 15:57:09 +0530

    Time: 0 ms (100%), Space: 41.7 MB (45.6%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -1,12 +1,22 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-    int[] freq = new int[26];
-    for(int i:sentence.toCharArray()){
-        freq[i-'a']++;
-    }  
-    for(int j : freq){
-        if(j==0)
+    // int[] freq = new int[26];
+    // for(int i:sentence.toCharArray()){
+    //     freq[i-'a']++;
+    // }  
+    // for(int j : freq){
+    //     if(j==0)
+    //         return false;
+    // }
+    // return true;
+    
+    if(sentence.length()<26)
+        return false;
+    
+    for(char c ='a'; c<='z'; c++){
+        if(sentence.indexOf(c)==-1)
             return false;
     }
-    return true;}
+    return true;
+    }
 }
