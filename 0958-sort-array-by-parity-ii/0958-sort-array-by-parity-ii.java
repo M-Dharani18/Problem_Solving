beb9641de04b39d7d8dcad07018c6f84fdc26f3f
Dharani M <dharani39393@gmail.com>
2025-08-28 21:25:46 +0530

    Time: 2 ms (99.01%), Space: 46.3 MB (98.4%) - LeetHub

--- 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.java
+++ 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.java
@@ -1,54 +1,50 @@
 class Solution {
     public int[] sortArrayByParityII(int[] nums) {
-        int e=0,o=1;
-        int n=nums.length;
-        while(e<n && o<n){
-            if((nums[e]%2)==0){
-                e+=2;}
-            else if((nums[o]%2)==1){
-                o+=2;
+//         int e=0,o=1;
+//         int n=nums.length;
+//         while(e<n && o<n){
+//             if((nums[e]%2)==0){
+//                 e+=2;}
+//             else if((nums[o]%2)==1){
+//                 o+=2;
+//             }
+//             else{
+//                 int temp=nums[e];
+//                 nums[e]=nums[o];
+//                 nums[o]=temp;
+//                 e+=2;
+//                 o+=2;
+//             }
+//         }
+//         return nums;
+//     }
+// }
+        int[] res = new int[nums.length];
+        int siz=nums.length;
+        int[] odd = new int[siz/2];
+        int[] even = new int[siz/2];
+        int e=0;
+        int o=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2==0){
+                even[e]=nums[i];
+                e++;
             }
             else{
-                int temp=nums[e];
-                nums[e]=nums[o];
-                nums[o]=temp;
-                e+=2;
-                o+=2;
+                odd[o]=nums[i];
+                o++;
             }
         }
-        return nums;
+        for(int m=0,a=0,b=0;m<nums.length;m++){
+            if((m%2)==0){
+                res[m]=even[a];
+                a++;
+            }
+            else{
+                res[m]=odd[b];
+                b++;
+            }
+        }
+        return res;
     }
 }
-        
-        
-        
-        
-        
-        
-        // int[] res = new int[nums.length];
-        // int siz=nums.length;
-        // int[] odd = new int[siz/2];
-        // int[] even = new int[siz/2];
-        // int e=0;
-        // int o=0;
-        // for(int i=0;i<nums.length;i++){
-        //     if(nums[i]%2==0){
-        //         even[e]=nums[i];
-        //         e++;
-        //     }
-        //     else{
-        //         odd[o]=nums[i];
-        //         o++;
-        //     }
-        // }
-        // for(int m=0,a=0,b=0;m<nums.length;m++){
-        //     if((m%2)==0){
-        //         res[m]=even[a];
-        //         a++;
-        //     }
-        //     else{
-        //         res[m]=odd[b];
-        //         b++;
-        //     }
-        // }
-        // return res;
