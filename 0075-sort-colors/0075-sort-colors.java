4373b2ed1353621150262a3d79b0beca5787d033
Dharani M <dharani39393@gmail.com>
2025-09-17 12:05:05 +0530

    Time: 0 ms (100%), Space: 42.1 MB (47.81%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -8,41 +8,28 @@ class Solution {
         //             nums[i]=temp;
         //         }
         //     }
-       int r=0,w=0,b=0;
-       for(int num:nums){
-        if(num ==0) r++;
-        if(num ==1) w++;
-        if(num ==2) b++;
-       }
-       int i=0;
-        while(r-- >0)
-            nums[i++]=0;
-        while(w-- >0)
-            nums[i++]=1;
-        while(b-- >0)
-            nums[i++]=2;
 
-    // int j=0;
-    //     int count=0,  count1=0, count2=0;
-    //     for(int i=0;i<nums.length;i++){
-    //         if(nums[i]==0){
-    //             count++;
-    //         }
-    //          if(nums[i]==1){
-    //             count1++;
-    //         }
-    //          if(nums[i]==2){
-    //             count2++;
-    //         }
-    //     }
-    //     for(int i=0;i<count;i++){
-    //         nums[j++]=0;
-    //     }
-    //       for(int i=0;i<count1;i++){
-    //         nums[j++]=1;
-    //     }
-    //       for(int i=0;i<count2;i++){
-    //         nums[j++]=2;
-    //     }
-       }
+        int j=0;
+        int c=0,c1=0,c2=0;
+        for(int num : nums){
+            if(num == 0){
+                c++;
+            }
+             if(num == 1){
+                c1++;
+            }
+             if(num == 2){
+                c2++;
+            }
+        } 
+        for(int i=0;i<c;i++){
+            nums[j++]=0;
+        }
+        for(int i=0;i<c1;i++){
+            nums[j++]=1;
+        }
+        for(int i=0;i<c2;i++){
+            nums[j++]=2;
+        }
     }
+}
