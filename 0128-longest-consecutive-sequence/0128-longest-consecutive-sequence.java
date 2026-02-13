8d5ec48d1f52351703988cb65fc47b7d6a7eccb4
Dharani M <dharani39393@gmail.com>
2026-02-13 19:38:34 +0530

    Time: 25 ms (81.97%), Space: 77.9 MB (82.16%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -19,21 +19,42 @@ class Solution {
         //     longest=Math.max(longest,c);
         // }
         // return longest;
+        // if(nums.length==0) return 0;
+        // Set<Integer> s = new HashSet<>();
+        // for(int i:nums) s.add(i);
+        // int longest=0;
+        // for(int i:s){
+        //     if(!s.contains(i-1)){
+        //     int curr=i;
+        //     int count=1;
+        //     while(s.contains(curr+1)){
+        //         curr++;
+        //         count++;
+        //     }
+        //     longest=Math.max(longest,count);
+        //     }
+        // }
+        // return longest;
+
+        int count=1,max=1;
+        int n=nums.length;
         if(nums.length==0) return 0;
-        Set<Integer> s = new HashSet<>();
-        for(int i:nums) s.add(i);
-        int longest=0;
-        for(int i:s){
-            if(!s.contains(i-1)){
-            int curr=i;
-            int count=1;
-            while(s.contains(curr+1)){
-                curr++;
-                count++;
-            }
-            longest=Math.max(longest,count);
-            }
+        List<Integer> l = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i=1;i<n;i++){
+            if(nums[i]!=nums[i-1]) l.add(nums[i-1]);
         }
-        return longest;
+        l.add(nums[n-1]);
+        int[] res=new int[l.size()];
+        for(int i=0;i<res.length;i++){
+            res[i]=l.get(i);
+        }
+        for(int i=0;i<res.length-1;i++){
+            if(res[i]+1==res[i+1]) { count++; }
+            else count=1;
+
+            if(count>max) max=count;
+        }
+        return max;
     }
 }
