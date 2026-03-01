ca3e5dcbf6a45144c3623d1905de8cca475c28a2
Dharani M <dharani39393@gmail.com>
2026-03-01 12:27:10 +0530

    Time: 7 ms (54.75%), Space: 47.5 MB (71.98%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.java
@@ -1,22 +1,42 @@
 class Solution {
     public int countCharacters(String[] words, String chars) {
-        Map<Character,Integer> mp = new HashMap<>();
+        // Map<Character,Integer> mp = new HashMap<>();
+        // int sum=0;
+        // for(char ch:chars.toCharArray()){
+        //     mp.put(ch,mp.getOrDefault(ch,0)+1);
+        // }
+        // for(String s:words){
+        //     Map<Character,Integer> mp1 = new HashMap<>();
+        //     boolean valid=true;
+        //     for(char ch:s.toCharArray()){
+        //     mp1.put(ch,mp1.getOrDefault(ch,0)+1);
+        // }
+        //     for(char ch:mp1.keySet()){
+        //         if(mp1.get(ch)>mp.getOrDefault(ch, 0)) {
+        //         valid=false;
+        //         break;}
+        //     }
+        // if(valid) sum+=s.length();
+        // }
+        // return sum;
         int sum=0;
+        int[] freq = new int[26];
         for(char ch:chars.toCharArray()){
-            mp.put(ch,mp.getOrDefault(ch,0)+1);
+            freq[ch-'a']++;
         }
         for(String s:words){
-            Map<Character,Integer> mp1 = new HashMap<>();
             boolean valid=true;
+            int[] freq1 = new int[26];
             for(char ch:s.toCharArray()){
-            mp1.put(ch,mp1.getOrDefault(ch,0)+1);
-        }
-            for(char ch:mp1.keySet()){
-                if(mp1.get(ch)>mp.getOrDefault(ch, 0)) {
-                valid=false;
-                break;}
+            freq1[ch-'a']++;
             }
-        if(valid) sum+=s.length();
+            for(char c:s.toCharArray()){
+                if(freq1[c-'a']>freq[c-'a']) {
+                    valid=false;
+                    break;
+                }
+            }
+            if(valid) sum+=s.length();
         }
         return sum;
     }
