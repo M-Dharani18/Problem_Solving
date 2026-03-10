cb2527193bf82c9b0e39eca898afae9294617638
Dharani M <dharani39393@gmail.com>
2026-03-10 22:37:58 +0530

    Time: 4 ms (98.2%), Space: 98.6 MB (15.21%) - LeetHub

--- 2105-watering-plants-ii/2105-watering-plants-ii.java
+++ 2105-watering-plants-ii/2105-watering-plants-ii.java
@@ -4,33 +4,53 @@ class Solution {
         int waterA=capacityA;
         int waterB=capacityB;
         int n=plants.length;
-        if(n==1 && plants[0]<=capacityA) return 0;
-            for(int i=0;i<n/2;i++){
-                if(waterA<plants[i]){
-                    refill++;
-                    waterA=capacityA;
-                }
-                waterA-=plants[i];
+        // if(n==1 && plants[0]<=capacityA) return 0;
+        //     for(int i=0;i<n/2;i++){
+        //         if(waterA<plants[i]){
+        //             refill++;
+        //             waterA=capacityA;
+        //         }
+        //         waterA-=plants[i];
+        //     }
+        //     if(n%2==0){
+        //     for(int i=n-1;i>=n/2;i--){
+        //         if(waterB<plants[i]){
+        //             refill++;
+        //             waterB=capacityB;
+        //         }
+        //         waterB-=plants[i];
+        //     }
+        //     }
+        //     else{
+        //         for(int i=n-1;i>n/2;i--){
+        //         if(waterB<plants[i]){
+        //             refill++;
+        //             waterB=capacityB;
+        //             }
+        //             waterB-=plants[i];
+        //         }
+        //         if(waterA<plants[n/2] && waterB<plants[n/2]) refill++;
+        //     }
+        //     return refill;
+        int left=0,right=n-1;
+        while(left<right){
+            if(waterA<plants[left]){
+                refill++;
+                waterA=capacityA;
             }
-            if(n%2==0){
-            for(int i=n-1;i>=n/2;i--){
-                if(waterB<plants[i]){
-                    refill++;
-                    waterB=capacityB;
-                }
-                waterB-=plants[i];
+            waterA-=plants[left];
+            left++;
+            if(waterB<plants[right]){
+                refill++;
+                waterB=capacityB;
             }
-            }
-            else{
-                for(int i=n-1;i>n/2;i--){
-                if(waterB<plants[i]){
-                    refill++;
-                    waterB=capacityB;
-                    }
-                    waterB-=plants[i];
-                }
-                if(waterA<plants[n/2] && waterB<plants[n/2]) refill++;
-            }
-            return refill;
+            waterB-=plants[right];
+            right--;
+        }
+        if(left==right){
+            if(Math.max(waterA,waterB)<plants[left]) refill++;
+        }
+        return refill;
+
         }
     }
