a0a7e8908a99727fec0637fe6c380f9f937af501
Dharani M <dharani39393@gmail.com>
2026-04-05 21:04:14 +0530

    Time: 26 ms (34.22%), Space: 47.7 MB (17.15%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,20 +1,48 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        int l=0;
-        List<Integer> li = new ArrayList<>();
-        Map<Character,Integer> mp1 = new HashMap<>();
+        // int l=0;
+        // List<Integer> li = new ArrayList<>();
+        // Map<Character,Integer> mp1 = new HashMap<>();
+        // for(char ch:p.toCharArray()){
+        //     mp1.put(ch,mp1.getOrDefault(ch,0)+1);
+        // }
+
+        // Map<Character,Integer> mp2 = new HashMap<>();
+        // for(int r=0;r<s.length();r++){
+        //     if(r-l+1>p.length()){
+        //         mp2.put(s.charAt(l),mp2.get(s.charAt(l))-1);
+        //         if(mp2.get(s.charAt(l))==0) mp2.remove(s.charAt(l));
+        //         l++;
+        //     }
+        //     mp2.put(s.charAt(r),mp2.getOrDefault(s.charAt(r),0)+1);
+        //     if(mp1.equals(mp2)) li.add(l);
+        // }
+        // return li;
+
+        List<Integer> li = new LinkedList<>();
+        if(s.length()<p.length()) return li;
+        Map<Character,Integer> mp = new HashMap<>();
         for(char ch:p.toCharArray()){
-            mp1.put(ch,mp1.getOrDefault(ch,0)+1);
+             mp.put(ch,mp.getOrDefault(ch,0)+1);
         }
-        Map<Character,Integer> mp2 = new HashMap<>();
-        for(int r=0;r<s.length();r++){
-            if(r-l+1>p.length()){
-                mp2.put(s.charAt(l),mp2.get(s.charAt(l))-1);
-                if(mp2.get(s.charAt(l))==0) mp2.remove(s.charAt(l));
-                l++;
+        int counter = mp.size();
+        int l=0,r=0;
+        while(r<s.length()){
+            char c=s.charAt(r);
+            if(mp.containsKey(c)){
+                mp.put(c,mp.get(c)-1);
+                if(mp.get(c)==0) counter--;
             }
-            mp2.put(s.charAt(r),mp2.getOrDefault(s.charAt(r),0)+1);
-            if(mp1.equals(mp2)) li.add(l);
+            r++;
+            while(counter==0){
+                char temp=s.charAt(l);
+                if(mp.containsKey(temp)){
+                    mp.put(temp,mp.get(temp)+1);
+                    if(mp.get(temp) > 0) counter++;
+                }
+            if(r-l==p.length()) li.add(l);
+            l++;
+            }
         }
         return li;
     }
